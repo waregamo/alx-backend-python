224c95ed2b96524a18c2312720092838f4b9f7b2
waregamo <waregamoses20@gmail.com>
2025-07-03 17:38:31 +0300

    Add batch streaming and processing with yield

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -20,7 +20,10 @@ def stream_users_in_batches(batch_size):
     conn.close()
 
 def batch_processing(batch_size):
+    filtered_users = []
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
             if user["age"] > 25:
-                print(user)
+                filtered_users.append(user)
+    return filtered_users
+
