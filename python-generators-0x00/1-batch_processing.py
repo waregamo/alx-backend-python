642111f55c6a491e686937c607cc231ba775e913
waregamo <waregamoses20@gmail.com>
2025-07-03 17:00:38 +0300

    Add batch streaming and processing with yield

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+def stream_users_in_batches(batch_size):
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="3Xplore@",  # ← Replace with your actual password
+        database="ALX_prodev"
+    )
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+
+    while True:
+        batch = cursor.fetchmany(batch_size)
+        if not batch:
+            break
+        yield batch
+
+    cursor.close()
+    conn.close()
+
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            if user["age"] > 25:
+                print(user)
