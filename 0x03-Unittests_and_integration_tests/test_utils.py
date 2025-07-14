9eadf5b9f55b7b8e7bb0b9696fb048123e18e291
waregamo <waregamoses20@gmail.com>
2025-07-14 12:55:05 +0300

    Fix Task 0: Remove docstring from test_access_nested_map

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,7 +16,6 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """Test that access_nested_map returns correct result."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
@@ -52,5 +51,6 @@ class TestGetJson(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
 
 
