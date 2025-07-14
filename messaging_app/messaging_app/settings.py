934b55ff57a7241f97f48fd116e5244d8206459f
waregamo <waregamoses20@gmail.com>
2025-07-14 19:22:01 +0300

    Add DRF settings with IsAuthenticated and authentication classes

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -125,3 +125,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django REST Framework global settings
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+}
