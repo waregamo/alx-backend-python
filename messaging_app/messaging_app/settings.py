cd7591e9fb271489900533829798b8c70956b23f
waregamo <waregamoses20@gmail.com>
2025-07-14 19:38:50 +0300

    Define custom User model, Conversation, and Message models

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -125,6 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'chats.User'
+
 
 # Django REST Framework global settings
 REST_FRAMEWORK = {
