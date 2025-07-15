7402d8efc734304c08994b3d1b2fdbe9c7695d95
waregamo <waregamoses20@gmail.com>
2025-07-15 13:53:05 +0300

    Fix: include both DefaultRouter and NestedDefaultRouter

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path, include
+from rest_framework import routers  
 from rest_framework_nested.routers import NestedDefaultRouter 
 from .views import ConversationViewSet, MessageViewSet
 
-router = NestedDefaultRouter(parent_router=None, parent_prefix='')
 
-# Register the viewsets the usual way
-router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
+default_router = routers.DefaultRouter
+nested_router = NestedDefaultRouter(parent_router=None, parent_prefix='')
+
+# Register normally using nested_router
+nested_router.register(r'conversations', ConversationViewSet, basename='conversation')
+nested_router.register(r'messages', MessageViewSet, basename='message')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(nested_router.urls)),  
 ]
+
 
