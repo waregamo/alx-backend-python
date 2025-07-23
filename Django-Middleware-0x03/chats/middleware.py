c3da64ff2fe532829994a2c20a03881ab5491b38
waregamo <waregamoses20@gmail.com>
2025-07-24 00:09:58 +0300

    Add RequestLoggingMiddleware and RestrictAccessByTimeMiddleware

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,20 +1,39 @@
 # chats/middleware.py
 
 from datetime import datetime
+from django.http import HttpResponseForbidden
 import os
+
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        # Define log file path (in project root)
+        # Define log file path (store in root directory)
         self.log_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'requests.log')
 
     def __call__(self, request):
-        user = request.user if hasattr(request, 'user') and request.user.is_authenticated else 'Anonymous'
+        user = request.user.username if hasattr(request, 'user') and request.user.is_authenticated else 'Anonymous'
         log_message = f"{datetime.now()} - User: {user} - Path: {request.path}\n"
 
         with open(self.log_file, 'a') as file:
             file.write(log_message)
 
-        response = self.get_response(request)
-        return response
+        return self.get_response(request)
+
+
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        current_hour = datetime.now().hour
+        # Allow only between 6 PM and 9 PM (18:00 - 21:00)
+        if current_hour < 18 or current_hour >= 21:
+            return HttpResponseForbidden("Access to chat is only allowed between 6 PM and 9 PM.")
+        return self.get_response(request)
+
+
+
+
+
+
--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'chats.middleware.RequestLoggingMiddleware',
+    'chats.middleware.RestrictAccessByTimeMiddleware',
 
 ]
 
