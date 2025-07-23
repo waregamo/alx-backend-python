b24630a399148d28de19b78de623b0a10338b3d9
waregamo <waregamoses20@gmail.com>
2025-07-23 21:44:44 +0300

    Update views and permissions, handle Postman collection changes

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,11 +1,25 @@
 from rest_framework import permissions
 
-class IsParticipantOfConversation(permissions.BasePermission):
+class IsParticipantOrReadOnly(permissions.BasePermission):
     """
-    Custom permission to allow only participants of a conversation to access it.
+    Custom permission to ensure:
+    - Only authenticated users can access the API.
+    - Only conversation participants can send, view, update, or delete messages.
     """
 
+    def has_permission(self, request, view):
+        # Only allow access to authenticated users
+        return request.user and request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
-        # Ensure the request user is one of the participants
-        return request.user in obj.participants.all()
+        # Allow safe methods (GET, HEAD, OPTIONS) if the user is a participant
+        if request.method in permissions.SAFE_METHODS:
+            return request.user in [obj.sender, obj.receiver]
+
+        # Allow modifying methods (PUT, PATCH, DELETE) only for participants
+        if request.method in ["PUT", "PATCH", "DELETE"]:
+            return request.user in [obj.sender, obj.receiver]
+
+        # For other methods like POST
+        return request.user in [obj.sender, obj.receiver]
 
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,16 +1,17 @@
 # chats/views.py
-
 from rest_framework import viewsets, status, filters
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.status import HTTP_403_FORBIDDEN
 from django_filters.rest_framework import DjangoFilterBackend
 from django.contrib.auth import get_user_model
 
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer, UserSerializer
-from .permissions import IsParticipantOfConversation
+from .permissions import IsParticipantOrReadOnly
 from .filters import MessageFilter
-from .pagination import MessagePagination  # <-- Now this is valid
+from .pagination import MessagePagination
 
 User = get_user_model()
 
@@ -23,7 +24,7 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
 
 class ConversationViewSet(viewsets.ModelViewSet):
     serializer_class = ConversationSerializer
-    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
+    permission_classes = [IsAuthenticated, IsParticipantOrReadOnly]
     filter_backends = [filters.SearchFilter]
     search_fields = ['participants__username']
 
@@ -33,13 +34,19 @@ class ConversationViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+
+        # Check if current user is part of the participants
+        participants = serializer.validated_data.get('participants')
+        if request.user not in participants:
+            return Response({"detail": "You must be a participant to create this conversation."}, status=HTTP_403_FORBIDDEN)
+
         self.perform_create(serializer)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
-    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
+    permission_classes = [IsAuthenticated, IsParticipantOrReadOnly]
     pagination_class = MessagePagination
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_class = MessageFilter
@@ -49,8 +56,19 @@ class MessageViewSet(viewsets.ModelViewSet):
         return Message.objects.filter(conversation__participants=self.request.user)
 
     def create(self, request, *args, **kwargs):
+        conversation_id = request.data.get('conversation_id')
+        if not self.user_in_conversation(conversation_id):
+            return Response({"detail": "You are not part of this conversation."}, status=HTTP_403_FORBIDDEN)
+
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
+        serializer.save(sender=request.user)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def user_in_conversation(self, conversation_id):
+        try:
+            conversation = Conversation.objects.get(id=conversation_id)
+            return self.request.user in conversation.participants.all()
+        except Conversation.DoesNotExist:
+            return False
 
