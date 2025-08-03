1250b152fd4cde52d22c5b2a3e337c638adb20ec
waregamo <waregamoses20@gmail.com>
2025-08-03 10:41:50 +0300

    Add full messaging system: edit history, delete user, threaded replies, unread filter, ORM optimizations, caching

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -14,13 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("inbox/", views.inbox, name="inbox"),
-    path("message/<int:message_id>/", views.view_thread, name="view_thread"),
-    path("send/", views.send_message, name="send_message"),
-    path("delete/", views.delete_user, name="delete_user"),
+    path('inbox/', views.inbox, name='inbox'),
+    path('thread/<int:message_id>/', views.thread_view, name='thread_view'),
+    path('delete/', views.delete_user, name='delete_user'),
 ]
+
