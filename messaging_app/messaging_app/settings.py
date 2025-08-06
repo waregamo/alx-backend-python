3847eb6bc54708bc1e771a067c31241df90ac96b
waregamo <waregamoses20@gmail.com>
2025-08-06 19:53:19 +0300

    Task 1: Added Docker Compose setup with MySQL and env variables

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -79,12 +79,19 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DATABASE'),
+        'USER': os.getenv('MYSQL_USER'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': os.getenv('MYSQL_HOST', 'db'),
+        'PORT': os.getenv('MYSQL_PORT', '3306'),
     }
 }
+
 
 
 # Password validation
